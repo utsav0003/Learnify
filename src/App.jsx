71a1c056702816be7b61768be91a45434435c4c1
Utsav <utsavjha9958@gmail.com>
2023-11-18 14:10:13 +0530

    Redux basic Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import {Routes} from "react-router-dom"
 
 function App() {
 
   return (
     <>
         <h1>Jai Shree Ram</h1>
+        <Routes>
+          
+        </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import store from './Redux/store'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import Toaster from "react-hot-toast"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <Provider store={store}>
+        <BrowserRouter>
+        <App />
+        <Toaster/>
+    </BrowserRouter>
+    </Provider>
 )
